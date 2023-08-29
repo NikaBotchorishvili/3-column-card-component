e0569cce094d151be9b0bea223ae34d871d38525
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2023-08-29 15:28:06 +0400

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,18 @@ module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
 		colors: {
-            brightOrange: "hsl(31, 77%, 52%)",
-            darkCyan: "hsl(184, 100%, 22%)",
-            veryDarkCyan: "hsl(179, 100%, 13%)",
-            transparentWhite: "hsla(0, 0%, 100%, 0.75)",   // paragraphs
-            veryLightGray: "hsl(0, 0%, 95%)",  // background, headings, buttons
-        },
-		extend: {
-            bigShoulders: "'Big Shoulders Display', cursive",
-            lexend: "'Lexend Deca', sans-serif",
-        },
+			brightOrange: "hsl(31, 77%, 52%)",
+			darkCyan: "hsl(184, 100%, 22%)",
+			veryDarkCyan: "hsl(179, 100%, 13%)",
+			transparentWhite: "hsla(0, 0%, 100%, 0.75)", // paragraphs
+			veryLightGray: "hsl(0, 0%, 95%)", // background, headings, buttons
+            white: "#fff",
+		},
+		fontFamily: {
+			bigShoulders: "'Big Shoulders Display', cursive",
+			lexend: "'Lexend Deca', sans-serif",
+		},
+		extend: {},
 	},
 	plugins: [],
 };
